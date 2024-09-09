d5eeb5d2fe218a1213e1f9daf102d8b51d154d90
Christian Staggs <christianstaggs@Christians-MacBook-Air.local>
2024-09-08 23:15:10 -0500

    Hi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int numberOfSandwiches = GetNumberSandwiches();
+int numberOfToppings = GetNumberToppings();
+double tip = GetTip();
+const double costOfSandwich = 4.75;
+const double costOfTopping = .55;
+const double discount = .10;
+double totalCostOfSandwich = TotalCostOfSandwich();
+double totalCostOfToppings = TotalCostOfToppings();
+double baseCost = totalCostOfSandwich + totalCostOfToppings;
+
+static int GetNumberSandwiches() {
+Console.WriteLine("How many sandwiches?");
+return int.Parse(Console.ReadLine());
+}
+
+static int GetNumberToppings() {
+Console.WriteLine("How many toppings?");
+return int.Parse(Console.ReadLine());
+}
+
+static double TotalCostOfSandwich() {
+    costOfSandwich * numberOfSandwiches
+}
+
+static double TotalCostOfToppings() {
+    costOfTopping * numberOfSandwiches
+}
+
+static double GetTip() {
+    Console.WriteLine("What percentage would you like to tip?");
+    return int.Parse(Console.ReadLine());
+}
+
+static double OrderCost(){
+    return (tip + baseCost(1-discount));
+}
